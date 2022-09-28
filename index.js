eecf2f7964a762dcf2e4062d588f20564dbef301
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-09-28 05:24:24 +0300

    Init the backend

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const mongoString = process.env.DATABASE_URL
+const routes = require('./routes/routes')
+
+mongoose.connect(mongoString)
+const database = mongoose.connection
+const app = express()
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected')
+})
+
+app.use(express.json())
+app.use('/api', routes)
+
+app.listen(5500, () => {
+  console.log(`Server Started at ${5500}`)
+})
